72f469194fb748495d109948bfbe53f2b6be7add
apip <apip@gitlab.lab>
2021-09-09 14:19:46 +0700

    adding navbar

--- fish-app/src/App.js
+++ fish-app/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const app = () => {
-  return <Router>
-    <Switch>
-      <Route path="/" exact><Users /></Route>
-      <Route path="/places/new" exact><NewPlace /></Route>
-      <Redirect to="/" />
-    </Switch>
-  </Router>; 
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 };
 
 export default app;
