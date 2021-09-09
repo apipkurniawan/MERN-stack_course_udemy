18309530c8fa580be93cb8fcad177db1d65c20a7
apip <apip@gitlab.lab>
2021-09-09 09:50:31 +0700

    create configuration

---
+++ fish-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const app = () => {
+  return <Router>
+    <Switch>
+      <Route path="/" exact><Users /></Route>
+      <Route path="/places/new" exact><NewPlace /></Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>; 
+};
+
+export default app;
